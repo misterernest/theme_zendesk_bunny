2dbd10506216ab8e9e4fd9cdc4057b2f81f14325
Ernesto Campos Villa <ecamposvilla@summasolutions.net>
2020-08-17 18:06:36 -0500

    Maquetado header con estilos

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ document.addEventListener('DOMContentLoaded', function() {
     toggle.focus();
   }
 
-  var burgerMenu = document.querySelector('.header .menu-button');
+  /*var burgerMenu = document.querySelector('.header .menu-button');
   var userMenu = document.querySelector('#user-nav');
 
   burgerMenu.addEventListener('click', function(e) {
@@ -162,7 +162,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   if (userMenu.children.length === 0) {
     burgerMenu.style.display = 'none';
-  }
+  }*/
 
   // Toggles expanded aria to collapsible elements
   var collapsible = document.querySelectorAll('.collapsible-nav, .collapsible-sidebar');
